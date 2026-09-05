f037f930350a3ee9b8bd84d32d73c2cd1011b7d1
Soumil <131894442+soumiil@users.noreply.github.com>
2026-09-05 19:50:53 +0530

    refactor: update CORS configuration and improve server startup logging , using dev script to load env before node starts the application

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+    origin: process.env.CORS_ORIGIN
 }));
+console.log('CORS ORIGIN APP.JS',process.env.CORS_ORIGIN)
 app.use(express.json({
     limit: "16kb"
 }))
 app.use(urlencoded());
 app.use(express.static("public"));
 app.use(cookieParser());
+
+app.get('/api/test', (req, res) => {
+    res.json("Hello World")
+})
 
 export default app;
--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-import dotenv from "dotenv"
 import connectDB from "./src/db/index.js"
 import app from "./app.js";
 
-dotenv.config();
-
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
     try {
         await connectDB();
         console.log('DATABASE CONNECTED!')
-        app.listen(process.env.PORT, () => {
-            console.log(`SERVER IS LISTENING AT PORT: http://localhost:${process.env.PORT}`);
+        app.listen(PORT, () => {
+            console.log(`SERVER IS LISTENING AT PORT: http://localhost:${PORT}`);
         })
     } catch(err) {
         console.log(`DATABASE CONNECTION FAILED: ${err}`);
